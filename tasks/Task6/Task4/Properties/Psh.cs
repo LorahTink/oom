c274409785e33af0221707e9970555a6bd9af4d2
TheArtsyFox <laura.battisti@kabsi.at>
2018-04-16 09:42:14 +0200

    add project Task6

---
+++ tasks/Task6/Task4/Properties/Psh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using Task6;
+
+namespace Task4.Properties
+{
+    public static class PushingCats
+    {
+        public static void Run()
+        {
+            Subject<Cat> subject = new Subject<Cat>();
+            var subscription = subject.Subscribe(
+                                     x => Console.WriteLine("Value published: {0}", x),
+                                     () => Console.WriteLine("Sequence Completed."));
+            /*subject.OnNext(1);
+
+            subject.OnNext(2);
+            */
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            subject.OnCompleted();
+            subscription.Dispose();
+        }
+    }
+}
---
+++ tasks/Task6/Task4/Properties/Task.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace lesson6
+{
+    public static class TasksExample
+    {
+        public static void Run()
+        {
+            var random = new Random();
+
+            var xs = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var tasks = new List<Task<int>>();
+
+            foreach (var x in xs)
+            {
+                var task = Task.Run(() =>
+                {
+                    WriteLine($"[T] computing result for {x}");
+                    Task.Delay(TimeSpan.FromSeconds(5.0 + random.Next(10))).Wait();
+                    WriteLine($"[T] done computing result for {x}");
+                    return x * x;
+                });
+
+                tasks.Add(task);
+            }
+
+            WriteLine("doing something else ...");
+
+            var tasks2 = new List<Task<int>>();
+            foreach (var task in tasks)
+            {
+                tasks2.Add(
+                    task.ContinueWith(t => { WriteLine($"[C] result is {t.Result}"); return t.Result; })
+                );
+            }
+
+            var cts = new CancellationTokenSource();
+            var primeTask = ComputePrimes(cts.Token);
+
+            ReadLine();
+            cts.Cancel();
+            WriteLine("canceled ComputePrimes");
+
+            ReadLine();
+        }
+
+        public static Task<bool> IsPrime(int x, CancellationToken ct)
+        {
+            return Task.Run(() =>
+            {
+                for (var i = 2; i < x - 1; i++)
+                {
+                    ct.ThrowIfCancellationRequested();
+                    if (x % i == 0) return false;
+                }
+                return true;
+            }, ct);
+        }
+
+        public static async Task ComputePrimes(CancellationToken ct)
+        {
+            for (var i = 100000000; i < int.MaxValue; i++)
+            {
+                ct.ThrowIfCancellationRequested();
+                if (await IsPrime(i, ct)) WriteLine($"[P] prime number: {i}");
+            }
+        }
+    }
+}
